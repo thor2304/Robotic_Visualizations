f7ac1b09e46046c1c74da8bcab1b3c3f96a4b8fc
Thor Jørgin <tjoer21@student.sdu.dk>
2023-10-29 19:24:14 +0100

    Improved the usability of the upload page

--- Maintenance/file_upload/uploadPageHydration.js
+++ Maintenance/file_upload/uploadPageHydration.js
@@ -1,12 +1,12 @@
-import {load, loadJson, remove} from "./Cache.js";
+import {loadJson, remove} from "./Cache.js";
 import {dataFileName} from "../helper_scripts/load_csv_data.js";
 import {scriptFileName} from "../source_code_visualization/fetch_and_render_sample_code.js";
 
 
 async function clearCaches(){
-    console.log(await loadJson("motorParams.json"))
+    // console.log(await loadJson("motorParams.json"))
     await remove(dataFileName)
     await remove(scriptFileName)
 }
 
-clearCaches().then(() => console.log("Cleared caches"))
+document.getElementById("clear-cache-button").addEventListener("click", clearCaches)
